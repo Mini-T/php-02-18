1f2e0fea2c9e044aa981709d1b270e7440fbf9b2
Mini-T <Minilucker.74@gmail.com>
2021-03-02 21:04:29 +0100

    test

--- quizz group/quizz.php
+++ quizz group/quizz.php
@@ -1,44 +1,50 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <?php
 
-class question{
-    public $content;
-    public $reponse;
+    class question
+    {
+        public $content;
+        public $reponse;
+    }
 
-}
+    $q1 = new question;
+    $q1->content = "que disent 2 fesses dans une pirogue ?";
+    $q1->reponse = "dis-donc ça commence à ramer du cul là non ?";
 
-$q1 = new question;
-$q1->content = "que disent 2 fesses dans une pirogue ?";
-$q1->reponse = "dis-donc ça commence à ramer du cul là non ?";
+    $q2 = new question;
+    $q2->content = "qu'est ce qu'une chauve souris avec une perruque ?";
+    $q2->reponse = "Une souris.";
 
-$q2 = new question;
-$q2->content = "qu'est ce qu'une chauve souris avec une perruque ?";
-$q2->reponse = "Une souris.";
-
-?>
-<form method="POST" action="">
-    <?php echo $q1->content?>
-    
-    <input type="text" name="r1" id="r1" placeholder="Votre réponse">
-    <input type="submit" name="envoyer" id="envoyer">
-    
-    <?php 
-    echo $_POST['r1'];
-    if($_POST['r1'] == $q1->reponse){
-        echo "bonne réponse";
-        }
     ?>
+    <form method="POST" action="">
+        <?php echo $q1->content ?> <br>
 
-    
+        <input type="text" name="r1" id="r1" placeholder="Votre réponse" style="width: 400px;" > <br>
+        <input type="submit" name="envoyer" id="envoyer"> <br>
 
-</form>
+        <?php
+        if(!empty($_POST))
+        echo $_POST['r1'];
+        if ($_POST['r1'] == $q1->reponse) {
+            echo "bonne réponse";
+        } elseif($_POST['r1'] != $q1->reponse) {
+            echo "imagine t'es nul a ce point";
+        }
+        ?>
+
+
+
+    </form>
 </body>
+
 </html>
